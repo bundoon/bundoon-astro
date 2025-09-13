f9edd74fdcd80c4fdf0022b3ca83eafeb467cba6
psham <dolanspirit@gmail.com>
2025-09-13 20:31:26 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
+import remarkGfm from 'remark-gfm';
 
 export default defineConfig({
-  output: 'static'
+  markdown: { remarkPlugins: [remarkGfm] },
 });
