d10968f6181f2a034bec3dc9d0676bac6dc91162
psham <dolanspirit@gmail.com>
2025-09-14 10:23:13 +0100

    Update auth.ts

--- functions/api/auth.ts
+++ functions/api/auth.ts
@@ -1,11 +1,12 @@
-export const onRequestGet: PagesFunction = async ({ env }) => {
-  const params = new URLSearchParams({
-    client_id: env.GITHUB_CLIENT_ID,
-    redirect_uri: env.OAUTH_REDIRECT_URI,
-    scope: "repo,user:email"
-  });
-  return Response.redirect(
-    `https://github.com/login/oauth/authorize?${params.toString()}`,
-    302
-  );
+export const onRequestGet = ({ request, env }) => {
+  const url = new URL(request.url);
+  const redirectUri = `${url.origin}/api/callback`;
+
+  // You can add state/nonce if you want, but Decap doesn't require it.
+  const auth = new URL('https://github.com/login/oauth/authorize');
+  auth.searchParams.set('client_id', env.GITHUB_CLIENT_ID);
+  auth.searchParams.set('redirect_uri', redirectUri);
+  auth.searchParams.set('scope', 'repo,user:email');
+
+  return Response.redirect(auth.toString(), 302);
 };
